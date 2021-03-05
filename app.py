29c9f89dd1397280bb684986a7b09fd37025657e
Habibah Adam <hebibe2s@gmail.com>
2021-03-05 12:10:36 +1100

    registration form added

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '3ea982a71d379d837581fda1bf6ecee4'
 
 
 @app.route('/')
@@ -7,9 +10,23 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
